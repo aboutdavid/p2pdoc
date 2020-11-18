809ef6ec5b7889efbea59d47d0fdc25bb5519472
Glitch (hello-webpage) <none>
2020-11-18 22:29:21 +0000

    🌖👬 Checkpoint ./loader.js:8045597/216 ./runtime/install.js:8045597/1920

--- runtime/install.js
+++ runtime/install.js
@@ -20,6 +20,13 @@ function run(packages) {
 
   cmd.on("close", code => {
     console.log(`child process exited with code ${code}`.toString());
+    if (config.runAfterInstall && config.runCmdAfterInstall) {
+      var cmdarray = config.runAfterInstall.split(" ");
+      var cmd = cmdarray[0];
+      var args = cmdarray.slice(1);
+      require("./runtime/exec.js").run(cmd, args);
+    }
   });
 }
+
 module.exports = { run };
