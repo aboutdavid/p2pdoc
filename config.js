b4a1582908fbd4eb30c63f3c1510b3fa7b3a4634
Glitch (hello-webpage) <none>
2020-12-01 23:54:53 +0000

    😼💁 Checkpoint ./config.js:8045597/441 ./index.html:8045597/2745

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  installPackages: true, // boolean, should the runtime install packages from npm?
-  packages: ["@editorjs/editorjs"], // Array, npm pacakges that should be installed if "installPackages" is set to true
-  runCmdAfterInstall: false, // boolean, Should the runtime run a command after package install?
-  runAfterInstall: null // string, command that should be ran if "runCmdAfterInstall" is set to true
+  installPackages: true,
+  packages: ["@editorjs/editorjs", "@editorjs/quote", "@editorjs/inline-code", "@editorjs/underline", "@editorjs/marker"],
+  runCmdAfterInstall: false,
+  runAfterInstall: "echo 'Finished installing!'"
 };
