fa04db2392a7fa5f645ca6c19250ef60a8606315
Glitch (hello-webpage) <none>
2020-12-02 02:29:56 +0000

    🚄🏎 Checkpoint ./script.js:8045597/5555 ./index.html:8045597/157 ./sw.js:8045597/678 ./.glitch-assets:8045597/385

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ document.addEventListener("DOMContentLoaded", async () => {
   window.node = node;
   if (window.location.hash.substr(1) != "") {
     (async () => {
-      for await (const data of window.node.cat(
-        window.location.hash.substr(1)
-      )) {
-        window.editor.render(JSON.parse(data.toString()));
-      }
+      window.loadDoc();
     })();
   }
   document.getElementById("savebtn").disabled = false;
   document.getElementById("savebtn").setAttribute("class", "block");
   document.getElementById("savebtn").innerHTML = "Save!";
 });
-
+window.loadDoc = async function() {
+  for await (const data of window.node.cat(window.location.hash.substr(1))) {
+    window.editor.render(JSON.parse(data.toString()));
+  }
+};
 window.saveDoc = function() {
   window.editor
     .save()
@@ -23,7 +23,7 @@ window.saveDoc = function() {
       (async () => {
         var res = JSON.stringify({ blocks: outputData.blocks });
         const { cid } = await window.node.add(res);
-
+        await window.node.pin.add(cid.string);
         navigator.clipboard
           .writeText(`https://${location.hostname}/#${cid.string}`)
           .then(
@@ -47,3 +47,16 @@ window.saveDoc = function() {
       console.log("Saving failed: ", error);
     });
 };
+var pwa = window.PWAinit();
+pwa.name = "P2Pdoc";
+pwa.short_name = "P2Pdoc";
+pwa.description =
+  "P2Pdoc allows you to create a document and then upload it to the distributed web!";
+pwa.icons = [
+  window.PWAIcon("/img/android-chrome-192x192.png", "192x192", "png"),
+  window.PWAIcon("/img/android-chrome-512x512.png", "512x512", "png"),
+  window.PWAIcon("/img/apple-touch-icon.png", "180x180", "png"),
+  window.PWAIcon("/img/favicon-16x16.png", "16x16", "png"),
+  window.PWAIcon("/img/favicon-32x32.png", "32x32", "png")
+];
+window.PWAInject(pwa, "/sw.js");
