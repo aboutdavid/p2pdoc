9714b47e3fd6fb13908cccb1b536e0ae77257691
Glitch (hello-webpage) <none>
2020-12-02 00:41:04 +0000

    👦🍁 Checkpoint ./config.js:8045597/433

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   installPackages: true,
-  packages: ["@editorjs/editorjs", "@editorjs/quote", "@editorjs/inline-code", "@editorjs/underline", "@editorjs/marker"],
+  packages: ["@editorjs/editorjs", "@editorjs/quote", "@editorjs/inline-code", "@editorjs/underline", "@editorjs/marker","@editorjs/list","@editorjs/code","@editorjs/table", "@editorjs/delimiter"],
   runCmdAfterInstall: false,
   runAfterInstall: "echo 'Finished installing!'"
 };
